219c1f6945770bc21e24ce0c6108befc3abc36fc
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2019-12-09 17:10:22 +0300

    Added tasks 1-4

---
+++ Task4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Car
+    {
+        public int speed { get; set; }
+        public event EventHandler speedEvent;
+        public void speedChanging(int newSpeed)
+        {
+            speed = newSpeed;
+            speedEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+    class SpeedMeter
+    {
+        private Car _car;
+        public SpeedMeter(Car car)
+        {
+            _car = car;
+            _car.speedEvent += speedView;
+        }
+        public void speedView(Object obj, EventArgs e)
+        {
+            Console.WriteLine("Current speed is : " + _car.speed);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bibiip = new Car();
+            SpeedMeter sp1 = new SpeedMeter(bibiip);
+
+            bibiip.speedChanging(80);
+            bibiip.speedChanging(130);
+
+            Console.ReadKey();
+        }
+    }
+}
